2afa607d9673826501ab20991006c83288feda7e
denervercosa <dener.vercosa@gmail.com>
2023-04-17 19:00:31 -0300

    atualizações Login

--- src/paginas/login/Login.tsx
+++ src/paginas/login/Login.tsx
@@ -2,8 +2,58 @@ import React from 'react'
 import {Grid, Box, Typography, TextField, Button} from '@mui/material'
 import {Link} from 'react-router-dom'
 import './Login.css'
+import UsuarioLogin from '../../models/UsuarioLogin'
+import { login } from '../../service/Service'
+import useLocalStorage from 'react-use-localstorage'
+
 
 function Login() {
+
+  // Hook responsável por navegar o usuário de uma tela para outra, sem precisar de um Link
+  const history = useNavigate()
+
+  // Hook customizado, para adicionar informações no LocalStorage do navegador
+  const [token, setToken] = useLocalStorage('token')
+
+  // Hook para controle de estado da Váriavel de UsuarioLogin, irá manter os dados de email e senha durante o preenchimento do formulário pelo usuário
+  const [userLogin, setUserLogin] = useState<UsuarioLogin>({
+    id: 0,
+    nome: '',
+    usuario: '',
+    foto: '',
+    senha: '',
+    token: ''
+  })
+
+  // função responsável por pegar o que foi digitado no campo, e atualizar o estado do Usuario
+  function updateModel(event: ChangeEvent<HTMLInputElement>) {
+    setUserLogin({
+      ...userLogin,
+      [event.target.name]: event.target.value
+    })
+  }
+
+  // Função responsável por enviar o pedido de login para a service do front, e consequentemente, para o backend. É uma função assincrona, pois precisa aguardar o backend devolver alguma resposta
+  async function onSubmit(event: ChangeEvent<HTMLFormElement>) {
+    event.preventDefault()
+    try {
+      await login('/usuarios/logar', userLogin, setToken)
+      alert('Usuario logado com sucesso')
+
+    } catch(error) {
+      console.log(error);
+      alert('Usuário ou senha inválidos')
+    }
+  }
+
+  // Hook de controle de "efeito colateral" que irá ficar monitorando a variavel token, e quando ela mudar, vai cair no if... caso seja verdadeiro, navega nosso usuário para a tela de Home
+  useEffect(() => {
+    if(token !== '') {
+      history('/home')
+    }
+  }, [token])
+    
+    
     return(
        <Grid container direction= "row" justifyContent={'center'} alignItems={'center'} className='fundo'>
             <Grid alignItems={'center'} xs={6}>
